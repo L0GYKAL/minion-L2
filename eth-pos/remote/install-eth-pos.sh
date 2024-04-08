0ce17e63f0f72924d42a6af8240f018e0b59e239
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-04-08 12:42:16 +0200

    fix(install-eth-pos.sh): replace pipx with python3-pip

--- eth-pos/remote/install-eth-pos.sh
+++ eth-pos/remote/install-eth-pos.sh
@@ -47,10 +47,9 @@ usage() {
 install_necessary_packages() {
   trap 'exit 1' ERR
   sudo apt-get update
-  sudo apt-get install -y git make build-essential python3 pipx python3-pip gcc g++ \
+  sudo apt-get install -y git make build-essential python3 python3-pip gcc g++ \
     cmake pkg-config llvm-dev libclang-dev clang protobuf-compiler jq
-  sudo pipx install web3 --include-deps
-  echo "pipx installed web3"
+  sudo python3-pip install web3
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
   source ${HOME}/.cargo/env
   trap - ERR
